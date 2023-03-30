0e0507aa55c193bf9059838c73dcf9cee0dc94e5
Teddy A. DeLeon (The Lion) <tdeleon@ai-codelabs.com>
2023-03-30 12:53:24 -0700

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -21,17 +21,17 @@ const Feedback = () => (
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-            Samantha
+            Ted Deleon
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-            Founder Metaverus
+            Founder Ai Code Labs
           </p>
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          “With the development of today's technology, metaverse is very
-          useful for today's work, or can be called web 3.0. by using
-          metaverse you can use it as anything”
+          “With the development of today's technology, Ai is very
+          useful for today's work, or can be called end all be all by using
+          A you can usedo anything”
         </p>
       </motion.div>
 
